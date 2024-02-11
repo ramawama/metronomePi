8bc20f24fcfe81285ca53ff28887b0109b17a619
Coomesicle <stephencoomes@ufl.edu>
2024-02-11 14:12:06 -0500

    :+1:  Added put and get

--- api/main.py
+++ api/main.py
@@ -2,17 +2,32 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/bpm/")
-def bpm():
-    return "bpm"
+@app.route("/bpm/<bpm>", methods=["PUT", "GET"])
+def get_bpm(bpm):
+    if request.method == "PUT":
+        data = request.get.json()
+        return jsonify(data), 201
+    if request.method == "GET":
+        bpm_data = {
+            "BPM"   :   bpm
+        }
+        return jsonify(bpm_data), 200
+    else:
+        return 403
 
-@app.route("/bpm/min/")
-def min():
-    return "min"
+@app.route("/bpm/min/<min>", methods=["DELETE", "GET"])
+def get_min(min):
+    bpm_min = {
+        "min"   :   min
+    }
+    return jsonify(bpm_min), 200
 
-@app.route("/bpm/max/")
-def max():
-    return "max"
+@app.route("/bpm/max/<max>", methods=["DELETE", "GET"])
+def get_max(max):
+    bpm_max =   {
+        "max"   :   max
+    }
+    return jsonify(bpm_max), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
