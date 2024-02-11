9e32c9aacef2ceaf91131f0f26fc41d140d39e6c
Coomesicle <stephencoomes@ufl.edu>
2024-02-11 12:31:47 -0500

    :airplane: Added all api routes

--- api/main.py
+++ api/main.py
@@ -2,9 +2,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/bpm")
-def home():
-    return "Home"
+@app.route("/bpm/")
+def bpm():
+    return "bpm"
+
+@app.route("/bpm/min/")
+def min():
+    return "min"
+
+@app.route("/bpm/max/")
+def max():
+    return "max"
 
 if __name__ == "__main__":
     app.run(debug=True)
