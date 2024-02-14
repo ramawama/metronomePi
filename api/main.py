727919b6355a6ea2a53564f8a45a09329a24112b
Coomesicle <stephencoomes@ufl.edu>
2024-02-13 19:29:53 -0500

    :barber: Cleaned up api

--- api/main.py
+++ api/main.py
@@ -2,51 +2,65 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# bpm json object
+# Dicts used to convert to json objects
 bpm = {"BPM" : None}
+min = {"Min" : None}
+max = {"Max" : None}
 
+
+# Uses put method to update BPM and get method to retrieve.
 @app.route("/bpm/", methods=["PUT", "GET"])
 def put_bpm():
-    # Put request 
+
     if request.method == "PUT":
         new_data = request.get_json()
         if ("BPM" in new_data):
             bpm["BPM"] = new_data["BPM"]
             return jsonify({"message": "BPM updated successfully"}), 200
-        return 400
-    # Checks if request was updated
-    if request.method == "GET":
-        if bpm["BPM"] is None:
-            return jsonify({'error': 'Key not found'}), 400
-        return jsonify(bpm), 200
-    return jsonify({"message":  "Not valid method"}), 400
+        return jsonify({"error": "JSON not given in correct format, use \"BPM\" as key."})
 
-min = {"Min" : None}
+    if request.method == "GET":
+        if bpm["BPM"] is not None:
+            return jsonify(bpm), 200
+        return jsonify({'message': 'Key currently not set.'}), 400
+    
+    return jsonify({"error":  "Not valid HTTP method."}), 400
+
+
+# Uses get method to retrieve minimum and delete to reset minimum.
 @app.route("/bpm/min/", methods=["DELETE", "GET"])
 def get_min():
+
     if request.method == "DELETE":
-        if(min["Min"] == None):
-            return jsonify({"message": "BPM already 0"}), 200
-        min["Min"] = None
-        return jsonify({"message": "BPM set to 0"}), 200
+        if(min["Min"] is not None):
+            min["Min"] = None
+            return jsonify({"message": "BPM reset to 0."}), 200
+        return jsonify({"message": "BPM already 0."}), 200
+
     if request.method == "GET":
-        if min["Min"] is None:
-            return jsonify({'error': 'Min not set'}), 400
-        return jsonify(min), 200
-    return jsonify({"message":  "Not valid method"}), 400
+        if min["Min"] is not None:
+            return jsonify(min), 200
+        return jsonify({'error': 'Minimum currently not set.'}), 400
     
-max = {"Max" : None}
+    return jsonify({"message":  "Not valid HTTP method."}), 400
+
+
+# Uses get method to retrieve maximum and delete to reset maximum.
 @app.route("/bpm/max/", methods=["DELETE", "GET"])
 def get_max():
+
     if request.method == "DELETE":
-        if(max["Max"] == None):
-            return jsonify({"message": "BPM already 0"}), 200
-        max["Max"] = None
-        return jsonify({"message": "BPM set to 0"}), 200
+        if(max["Max"] is not None):
+            max["Max"] = None
+            return jsonify({"message": "BPM reset to None."}), 200
+        return jsonify({"message": "BPM already None."}), 200
+    
     if request.method == "GET":
-        if max["Max"] is None:
-            return jsonify({'error': 'Max not set'}), 400
-        return jsonify(max), 200
-    return jsonify({"message":  "Not valid method"}), 400
+        if max["Max"] is not None:
+            return jsonify(max), 200
+        return jsonify({'error': 'Maximum currently not set.'}), 400
+    
+    return jsonify({"message":  "Not valid HTTP method."}), 400
+
 if __name__ == "__main__":
     app.run(debug=True)
